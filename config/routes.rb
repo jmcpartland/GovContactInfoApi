ffd3b32a93e9b7bee5615460b51f13edb66f1b7d
Joseph McPartland <joemcpartland@gmail.com>
2023-06-29 12:49:17 -0600

    create show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :members, only: [:index, :show]
+  get '/state/:state', to: 'members#members_by_state'
 
   # Defines the root path route ("/")
   # root "articles#index"
